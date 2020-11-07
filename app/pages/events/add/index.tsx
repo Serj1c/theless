fe2043e0948b7749b69d06739130882b5697afc5
Sergei Isaev <sergej.a.isaev@gmail.com>
2020-11-07 19:52:06 +0300

    Less 25 fix build (#19)
    Fixed SVGs imports for static render

--- app/pages/events/add/index.tsx
+++ app/pages/events/add/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { NextPage } from 'next';
 import { AddEventPage } from 'components/pages';
 
-const AddEventPageComponent = (): JSX.Element => <AddEventPage />;
+const AddEventPageComponent: NextPage = () => <AddEventPage />;
+
+// NOTE Temporary fix to disable static rendering
+export const getServerSideProps = async () => {
+  return;
+};
 
 export default AddEventPageComponent;
--- app/pages/profile/favorites/index.tsx
+++ app/pages/profile/favorites/index.tsx
@@ -16,4 +16,9 @@ const FavoritePage: NextPage = () => (
   </>
 );
 
+// NOTE Temporary fix to disable static rendering
+export const getServerSideProps = async () => {
+  return;
+};
+
 export default FavoritePage;
